459fdbd935d647c5545db764386f866445d0e87c
jpbourgeon <jeanphilippe.bourgeon@gmail.com>
2018-04-24 18:00:30 +0200

    Configure __testbed__

--- main/utils/pdfaggregator.script.js
+++ main/utils/pdfaggregator.script.js
@@ -1,9 +1,22 @@
+const fs = require('fs');
+const { resolve } = require('app-root-path');
 const aggregate = require('./pdfaggregator');
 
+const testfolder = 'main/utils/__testbed__';
+
+fs.readdir(resolve(`${testfolder}/output`), (err, files) => {
+  if (err) throw err;
+  files.forEach((file) => {
+    fs.unlink(resolve(`${testfolder}/output/${file}`), (err2) => {
+      if (err2) throw err2;
+    });
+  });
+});
+
 const options = {
-  input: 'D:\\Users\\bourgeonjp\\Documents\\eBooks',
-  output: 'D:\\Users\\bourgeonjp\\Documents\\eBooks',
-  logo: 'D:\\Users\\bourgeonjp\\Documents\\livethecode_logo\\logo.png',
+  input: resolve(`${testfolder}/input`),
+  output: resolve(`${testfolder}/output`),
+  logo: resolve(`${testfolder}/logo/image.jpg`),
   filename: '%dossiersource%_%dateiso%',
   title: '%dossiersource%%ligne%%datefr%',
   level: 0,
