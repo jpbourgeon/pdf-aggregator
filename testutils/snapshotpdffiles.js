42a1bdc986be0e5db1abd797d05d1ce89a1cd5ab
jpbourgeon <jeanphilippe.bourgeon@gmail.com>
2018-05-23 19:08:33 +0200

    fix tests (5)

--- testutils/snapshotpdffiles.js
+++ testutils/snapshotpdffiles.js
@@ -7,9 +7,15 @@ const snapshotPdfFiles = async (folder) => {
   pdfFiles = pdfFiles.filter(element => (element.substr(-4) === '.pdf'));
 
   const promises = pdfFiles.map(async (pdf) => {
-    const content = await fs.readFile(`${folder}/${pdf}`)
+    let content = await fs.readFile(`${folder}/${pdf}`)
       .catch(e => console.log(`snapshotPdfFiles > hash fs.readFile: ${e.message}`)); // eslint-disable-line no-console
     // Selection of the hashing algorithm:  https://medium.com/@chris_72272/what-is-the-fastest-node-js-hashing-algorithm-c15c1a0e164e
+    content = content
+      .toString('utf-8')
+      .replace(/(\/ID \[)(.*)(\])/, '$1ID$3')
+      .replace(/(\/Producer \()(.*)(\))/, '$1PRODUCER$3')
+      .replace(/(\/CreationDate \()(.*)(\))/, '$1DATE$3');
+    console.log(content);
     return crypto.createHash('sha1').update(content).digest('base64');
   });
 
