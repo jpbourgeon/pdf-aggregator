636153ea63508cc599097fbb0893674390df56d1
jpbourgeon <jeanphilippe.bourgeon@gmail.com>
2018-05-23 10:02:58 +0200

    Added: hash alternative to the snapshotpdffiles test util

--- main/utils/__testutils__/snapshotpdffiles.js
+++ main/utils/__testutils__/snapshotpdffiles.js
@@ -1,13 +1,14 @@
 const fs = require('fs-extra');
+const crypto = require('crypto');
 
-const snapshotPdfFiles = async (folder) => {
+const encode = async (folder) => {
   let pdfFiles = await fs.readdir(folder)
-    .catch(e => console.log(`snapshotPdfFiles > fs.readdir: ${e.message}`)); // eslint-disable-line no-console
+    .catch(e => console.log(`snapshotPdfFiles > encode fs.readdir: ${e.message}`)); // eslint-disable-line no-console
   pdfFiles = pdfFiles.filter(element => (element.substr(-4) === '.pdf'));
 
   const promises = pdfFiles.map(async (pdf) => {
     let content = await fs.readFile(`${folder}/${pdf}`)
-      .catch(e => console.log(`snapshotPdfFiles > fs.readFile: ${e.message}`)); // eslint-disable-line no-console
+      .catch(e => console.log(`snapshotPdfFiles > encode fs.readFile: ${e.message}`)); // eslint-disable-line no-console
     content = content
       .toString()
       .replace(/(\/Producer \()(.*)(\))/, '$1MOCKED_PRODUCER$3')
@@ -19,9 +20,35 @@ const snapshotPdfFiles = async (folder) => {
   });
 
   const snapshot = await Promise.all(promises)
-    .catch(e => console.log(`snapshotPdfFiles > Promise.all: ${e.message}`)); // eslint-disable-line no-console
+    .catch(e => console.log(`snapshotPdfFiles > encode Promise.all: ${e.message}`)); // eslint-disable-line no-console
 
   return snapshot.sort();
 };
 
-module.exports = snapshotPdfFiles;
+const hash = async (folder) => {
+  let pdfFiles = await fs.readdir(folder)
+    .catch(e => console.log(`snapshotPdfFiles > hash fs.readdir: ${e.message}`)); // eslint-disable-line no-console
+  pdfFiles = pdfFiles.filter(element => (element.substr(-4) === '.pdf'));
+
+  const promises = pdfFiles.map(async (pdf) => {
+    let content = await fs.readFile(`${folder}/${pdf}`)
+      .catch(e => console.log(`snapshotPdfFiles > hash fs.readFile: ${e.message}`)); // eslint-disable-line no-console
+    content = content
+      .toString()
+      .replace(/(\/Producer \()(.*)(\))/, '$1MOCKED_PRODUCER$3')
+      .replace(/(\/CreationDate \(D:)(.*)(\))/, '$1MOCKED_DATE$3')
+      .replace(/(\/ID \[)(.*)(\])/, '$1MOCKED_ID$3');
+    // Selection of the hashing algorithm:  https://medium.com/@chris_72272/what-is-the-fastest-node-js-hashing-algorithm-c15c1a0e164e
+    return crypto.createHash('sha1').update(content).digest('base64');
+  });
+
+  const snapshot = await Promise.all(promises)
+    .catch(e => console.log(`snapshotPdfFiles > hash Promise.all: ${e.message}`)); // eslint-disable-line no-console
+
+  return snapshot.sort();
+};
+
+module.exports = {
+  encode,
+  hash,
+};
--- main/utils/__testutils__/snapshotpdffiles.test.js
+++ main/utils/__testutils__/snapshotpdffiles.test.js
@@ -1,29 +1,55 @@
 const { resolve } = require('app-root-path');
-const snapshotPdfFiles = require('./snapshotpdffiles');
+const { encode, hash } = require('./snapshotpdffiles');
 
 const testbed = resolve('main/utils/__testbed__/snapshotpdffiles');
 
-describe('The test utility function snapshotPdfFiles', () => {
+describe('The test utility function encode', () => {
   it('should return an empty array from a folder without pdf files', async () => {
     expect.assertions(2);
-    const folderSnapshot = await snapshotPdfFiles(testbed)
-      .catch(e => console.log(`snapshotPdfFiles test 1: ${e.message}`)); // eslint-disable-line no-console
+    const folderSnapshot = await encode(testbed)
+      .catch(e => console.log(`snapshotPdfFiles > encode test 1: ${e.message}`)); // eslint-disable-line no-console
     expect(folderSnapshot).toHaveLength(0);
     expect(folderSnapshot).toMatchSnapshot();
   });
 
-  it('should snapshot a folder with a single pdf file into an array with one element', async () => {
+  it('should encode a folder with a single pdf file into an array with one element', async () => {
     expect.assertions(2);
-    const folderSnapshot = await snapshotPdfFiles(`${testbed}/onepdffile`)
-      .catch(e => console.log(`snapshotPdfFiles test 2: ${e.message}`)); // eslint-disable-line no-console
+    const folderSnapshot = await encode(`${testbed}/onepdffile`)
+      .catch(e => console.log(`snapshotPdfFiles > encode test 2: ${e.message}`)); // eslint-disable-line no-console
     expect(folderSnapshot).toHaveLength(1);
     expect(folderSnapshot).toMatchSnapshot();
   });
 
-  it('should snapshot a folder with multiple pdf files into an array', async () => {
+  it('should encode a folder with multiple pdf files into an array', async () => {
     expect.assertions(2);
-    const folderSnapshot = await snapshotPdfFiles(`${testbed}/manypdffiles`)
-      .catch(e => console.log(`snapshotPdfFiles test 3: ${e.message}`)); // eslint-disable-line no-console
+    const folderSnapshot = await encode(`${testbed}/manypdffiles`)
+      .catch(e => console.log(`snapshotPdfFiles > encode test 3: ${e.message}`)); // eslint-disable-line no-console
+    expect(folderSnapshot).toHaveLength(3);
+    expect(folderSnapshot).toMatchSnapshot();
+  });
+});
+
+describe.only('The test utility function hash', () => {
+  it('should return an empty array from a folder without pdf files', async () => {
+    expect.assertions(2);
+    const folderSnapshot = await hash(testbed)
+      .catch(e => console.log(`snapshotPdfFiles > hash test 1: ${e.message}`)); // eslint-disable-line no-console
+    expect(folderSnapshot).toHaveLength(0);
+    expect(folderSnapshot).toMatchSnapshot();
+  });
+
+  it('should hash a folder with a single pdf file into an array with one element', async () => {
+    expect.assertions(2);
+    const folderSnapshot = await hash(`${testbed}/onepdffile`)
+      .catch(e => console.log(`snapshotPdfFiles > hash test 2: ${e.message}`)); // eslint-disable-line no-console
+    expect(folderSnapshot).toHaveLength(1);
+    expect(folderSnapshot).toMatchSnapshot();
+  });
+
+  it('should hash a folder with multiple pdf files into an array', async () => {
+    expect.assertions(2);
+    const folderSnapshot = await hash(`${testbed}/manypdffiles`)
+      .catch(e => console.log(`snapshotPdfFiles > hash test 3: ${e.message}`)); // eslint-disable-line no-console
     expect(folderSnapshot).toHaveLength(3);
     expect(folderSnapshot).toMatchSnapshot();
   });
