33dd36d603af31fb231f86f73c503be51a568c5d
jpbourgeon <jeanphilippe.bourgeon@gmail.com>
2018-05-24 19:36:45 +0200

    Updated: todos

--- todo.test.js
+++ todo.test.js
@@ -1,16 +1,16 @@
 describe('todo', () => {
   describe('app', () => {
-    it('should have an about modal dialog and a trigger button available on all pages');
-    it('should be localized (i18n): ui and main process messages');
+    it('should have an about modal dialog and a trigger button available on all pages', () => undefined);
+    it('should be localized (i18n): ui and main process messages', () => undefined);
   });
 
   describe('index page', () => {
-    it('should format and display the aggregation steps from the log');
-    it('should provide a way to save the job\'s log to a text file when the processing is over');
+    it('should format and display the aggregation steps from the log', () => undefined);
+    it('should provide a way to save the job\'s log to a text file when the processing is over', () => undefined);
   });
 
   describe('start page', () => {
-    it('should have a page number option');
-    it('should have a toc option');
+    it('should have a page number option', () => undefined);
+    it('should have a toc option', () => undefined);
   });
 });
