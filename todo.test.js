e030d2423d770bb2323d3763aef0b1bdeac4359a
jpbourgeon <jeanphilippe.bourgeon@gmail.com>
2018-05-24 19:44:03 +0200

    Updated: todos

--- todo.test.js
+++ todo.test.js
@@ -2,6 +2,8 @@ describe('todo', () => {
   describe('app', () => {
     it('should have an about modal dialog and a trigger button available on all pages', () => undefined);
     it('should be localized (i18n): ui and main process messages', () => undefined);
+    it('should have up-to-date packages (next-6.0.3, ...)', () => undefined);
+    it('should have a detailed documentation', () => undefined);
   });
 
   describe('index page', () => {
