7d94e2673b5024047a6af6b420535fd3c483609c
jpbourgeon <jeanphilippe.bourgeon@gmail.com>
2018-05-25 17:40:16 +0200

    Changed: todolist priorisation

--- todolist.spec.js
+++ todolist.spec.js
@@ -1,12 +1,12 @@
 describe('todo', () => {
   describe('app', () => {
-    it('should have an about modal dialog and a trigger button available on all pages', () => undefined);
-    it('should be localized (i18n): ui and main process messages', () => undefined);
     it('should have up-to-date packages (next-6.0.3, ...)', () => undefined);
+    it('should have an about modal dialog and a trigger button available on all pages', () => undefined);
     it('should have a detailed documentation', () => undefined);
+    it('should be localized (i18n): ui and main process messages', () => undefined);
   });
 
-  describe('index page', () => {
+  describe('result page', () => {
     it('should format and display the aggregation steps from the log', () => undefined);
     it('should provide a way to save the job\'s log to a text file when the processing is over', () => undefined);
   });
