e5abe8fcce199c85397cd05197c8d650ac293797
jpbourgeon <jeanphilippe.bourgeon@gmail.com>
2018-05-27 00:28:47 +0200

    Changed: update dependencies to use next@^6.0.3
    /!\ To compile on windows, I had to switch to a fork of electron-next
    Don't forget to come back to the main branch when the PR will have been accepted

--- backlog.spec.js
+++ backlog.spec.js
@@ -1,9 +1,9 @@
 describe('backlog', () => {
   describe('app', () => {
-    it('should have up-to-date packages (next-6.0.3, ...)', () => undefined);
     it('should have an about modal dialog and a trigger button available on all pages', () => undefined);
     it('should have a detailed documentation', () => undefined);
     it('should be localized (i18n): ui and main process messages', () => undefined);
+    it('may use Neutron instead of electron-next', () => undefined);
   });
 
   describe('result page', () => {
