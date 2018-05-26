5f429b938deeedbc2cf7daf242d17cb182422944
jpbourgeon <jeanphilippe.bourgeon@gmail.com>
2018-05-26 22:17:42 +0200

    Changed: rename todolist into backlog

--- backlog.spec.js
+++ backlog.spec.js
@@ -1,4 +1,4 @@
-describe('todo', () => {
+describe('backlog', () => {
   describe('app', () => {
     it('should have up-to-date packages (next-6.0.3, ...)', () => undefined);
     it('should have an about modal dialog and a trigger button available on all pages', () => undefined);
