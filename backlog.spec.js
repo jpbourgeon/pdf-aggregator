49b63dc0fafc0f988ccd83fd417a3fd7b2ce66f4
jpbourgeon <jeanphilippe.bourgeon@gmail.com>
2018-05-29 09:23:28 +0200

    Changed: backlog

--- backlog.spec.js
+++ backlog.spec.js
@@ -3,11 +3,11 @@ describe('backlog', () => {
     it('should have an about modal dialog and a trigger button available on all pages', () => undefined);
     it('should have a detailed documentation', () => undefined);
     it('should be localized (i18n): ui and main process messages', () => undefined);
-    it('may use Neutron instead of electron-next', () => undefined);
+    it('should let the user cancel the current running job at will', () => undefined);
   });
 
   describe('result page', () => {
-    it('should format and display the aggregation steps from the log', () => undefined);
     it('should provide a way to save the job\'s log to a text file when the processing is over', () => undefined);
   });
+
 });
