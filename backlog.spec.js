ec1b760d6f672656dbc2eedbcc290a23d88a0c06
jpbourgeon <jeanphilippe.bourgeon@gmail.com>
2018-05-30 09:36:48 +0200

    Changed: backlog

--- backlog.spec.js
+++ backlog.spec.js
@@ -5,9 +5,4 @@ describe('backlog', () => {
     it('should be localized (i18n): ui and main process messages', () => undefined);
     it('should let the user cancel the current running job at will', () => undefined);
   });
-
-  describe('result page', () => {
-    it('should provide a way to save the job\'s log to a text file when the processing is over', () => undefined);
-  });
-
 });
