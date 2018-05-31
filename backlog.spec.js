c43d225c8c312bcd48029bcede1f63d9e723542e
jpbourgeon <jeanphilippe.bourgeon@gmail.com>
2018-05-31 19:48:21 +0200

    Changed: backlog

--- backlog.spec.js
+++ backlog.spec.js
@@ -3,7 +3,14 @@ describe('backlog', () => {
     it('should have a detailed documentation', () => undefined);
     it('should be localized (i18n): ui and main process messages', () => undefined);
   });
+
   describe('code', () => {
     it('should have a refactor of filename deduplication', () => undefined);
+    it('should have a fixed interruption message - currently shows message x3', () => undefined);
+  });
+
+  describe('result pdf', () => {
+    it('should have a cleaner cover page (more spacing, better fonts)', () => undefined);
+    it('should not merge _blank files', () => undefined);
   });
 });
