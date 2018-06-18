96585e30cd6f8309d3e594d470803d761f84bf0d
jpbourgeon <jeanphilippe.bourgeon@gmail.com>
2018-06-18 14:27:40 +0200

    Changed: backlog

--- backlog.spec.js
+++ backlog.spec.js
@@ -4,7 +4,16 @@ describe('backlog', () => {
     it('should be localized (i18n): ui and main process messages', () => undefined);
   });
 
-  describe('the result pdf file', () => {
-    it('should display better outline entries (allow the same name for multiple entries)', () => undefined);
+  describe('Icebox', () => {
+    describe('package.json', () => {
+      it(
+        'should use the official rkusa/pdfjs package when PR #98 will have been merged and published',
+        () => undefined,
+      );
+      it(
+        'should use the official leo/electron-next package when PR #19 will have been merged and published',
+        () => undefined,
+      );
+    });
   });
 });
