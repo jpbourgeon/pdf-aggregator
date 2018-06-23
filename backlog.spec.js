e9442f9bcb2cd61665711ea3cb1cc45f0dd6ca49
jpbourgeon <jeanphilippe.bourgeon@gmail.com>
2018-06-23 15:36:17 +0200

    Removed: useless tests

--- backlog.spec.js
+++ backlog.spec.js
@@ -1,15 +1,10 @@
 describe('backlog', () => {
   describe('The app', () => {
     it('should have a detailed documentation', () => undefined);
-    it('should be localized (i18n): ui and main process messages', () => undefined);
   });
 
   describe('Icebox', () => {
     describe('package.json', () => {
-      it(
-        'should use the official rkusa/pdfjs package when PR #98 will have been merged and published',
-        () => undefined,
-      );
       it(
         'should use the official leo/electron-next package when PR #19 will have been merged and published',
         () => undefined,
